832e4657d4137a8bd69f36878e9aeed41bc1ed8c
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-03-05 00:48:09 +0700

    add middleware

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.json('response');
 });
-
+const logger = require('./middleware/logger');
+app.use(logger);
 app.listen('3000', () => {
     console.log('Port 3000');
 });
