93317193f0cc349c64aec906b04fbb2c8b8870f8
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-04-24 16:54:54 +0700

    use in route

--- routes/linemessage.js
+++ routes/linemessage.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const {WebhookClient} = require('dialogflow-fulfillment');
 const router = express.Router();
 const lineMessage = require('../controller/linemessage');
-
+const agent = new WebhookClient({request: req, response: res});
 router.get('/', (req, res) => {
-    lineMessage(req, res, next);
-    return res.status(200).json({
-        status: 'success'
-    });
+    console.log('POST: /');
+    console.log('Body: ',req.body);
+
+    function welcome(agent) {
+        console.log("---------------------line welcome---------------------------");
+        agent.add(`Welcome to my agent!`);
+    }
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    agent.handleRequest(intentMap);
+    
 });
 module.exports = router;
