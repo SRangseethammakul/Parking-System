93c1ee2749fc1213bd7151e740576a401f9a851e
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-04-24 16:57:24 +0700

    change method

--- routes/linemessage.js
+++ routes/linemessage.js
@@ -3,7 +3,7 @@ const {WebhookClient} = require('dialogflow-fulfillment');
 const router = express.Router();
 const lineMessage = require('../controller/linemessage');
 const agent = new WebhookClient({request: req, response: res});
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     console.log('POST: /');
     console.log('Body: ',req.body);
 
