a65005f7c836aee0ced67cc289ebf2dfeea549ac
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-04-24 17:04:36 +0700

    s

--- routes/linemessage.js
+++ routes/linemessage.js
@@ -1,19 +1,33 @@
 const express = require('express');
-const {WebhookClient} = require('dialogflow-fulfillment');
+const {
+    WebhookClient
+} = require('dialogflow-fulfillment');
 const router = express.Router();
 const lineMessage = require('../controller/linemessage');
 
 router.post('/', (req, res) => {
     console.log('POST: /');
-    console.log('Body: ',req.body);
-    const agent = new WebhookClient({request: req, response: res});
+    console.log('Body: ', req.body);
+    const agent = new WebhookClient({
+        request: req,
+        response: res
+    });
+
     function welcome(agent) {
         console.log("---------------------line welcome---------------------------");
+        let payloadJson = {
+            "type": "sticker",
+            "packageId": "11537",
+            "stickerId": "52002762"
+        };
+        let payload = new Payload("LINE", payloadJson, {
+            sendAsMessage: true
+        });
         agent.add(`Welcome to my agent!`);
+        agent.add(payload);	
     }
     let intentMap = new Map();
     intentMap.set('Default Welcome Intent', welcome);
     agent.handleRequest(intentMap);
-    
 });
 module.exports = router;
