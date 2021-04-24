035b0e9ce476903a837a6580ebc43ba6c428b9ac
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-04-24 17:22:45 +0700

    ss

--- routes/linemessage.js
+++ routes/linemessage.js
@@ -19,210 +19,202 @@ router.post('/', (req, res) => {
             "contents": {
                 "type": "bubble",
                 "body": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "RECEIPT",
-                      "weight": "bold",
-                      "color": "#1DB446",
-                      "size": "sm"
-                    },
-                    {
-                      "type": "text",
-                      "text": "Brown Store",
-                      "weight": "bold",
-                      "size": "xxl",
-                      "margin": "md"
-                    },
-                    {
-                      "type": "text",
-                      "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                      "size": "xs",
-                      "color": "#aaaaaa",
-                      "wrap": true
-                    },
-                    {
-                      "type": "separator",
-                      "margin": "xxl"
-                    },
-                    {
-                      "type": "box",
-                      "layout": "vertical",
-                      "margin": "xxl",
-                      "spacing": "sm",
-                      "contents": [
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
                         {
-                          "type": "box",
-                          "layout": "horizontal",
-                          "contents": [
-                            {
-                              "type": "text",
-                              "text": "Energy Drink",
-                              "size": "sm",
-                              "color": "#555555"
-                            },
-                            {
-                              "type": "text",
-                              "text": "$2.99",
-                              "size": "sm",
-                              "color": "#111111",
-                              "align": "end"
-                            }
-                          ]
+                            "type": "text",
+                            "text": "สถานะการณ์โควิด",
+                            "margin": "md",
+                            "size": "xxl",
+                            "style": "normal",
+                            "weight": "regular",
+                            "align": "start",
+                            "gravity": "center",
+                            "wrap": true,
+                            "color": "#4ecdc4"
                         },
                         {
-                          "type": "box",
-                          "layout": "horizontal",
-                          "contents": [
-                            {
-                              "type": "text",
-                              "text": "Chewing Gum",
-                              "size": "sm",
-                              "color": "#555555"
-                            },
-                            {
-                              "type": "text",
-                              "text": "$0.99",
-                              "size": "sm",
-                              "color": "#111111",
-                              "align": "end"
-                            }
-                          ]
+                            "type": "box",
+                            "layout": "horizontal",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "วันที่",
+                                    "color": "#aaaaaa"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "5",
+                                    "color": "#aaaaaa"
+                                }
+                            ],
+                            "spacing": "md",
+                            "margin": "xl"
                         },
                         {
-                          "type": "box",
-                          "layout": "horizontal",
-                          "contents": [
-                            {
-                              "type": "text",
-                              "text": "Bottled Water",
-                              "size": "sm",
-                              "color": "#555555"
-                            },
-                            {
-                              "type": "text",
-                              "text": "$3.33",
-                              "size": "sm",
-                              "color": "#111111",
-                              "align": "end"
-                            }
-                          ]
+                            "type": "separator",
+                            "margin": "xxl"
                         },
                         {
-                          "type": "separator",
-                          "margin": "xxl"
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "ข้อมูลประจำวัน",
+                                    "align": "center"
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "ติดเชื้อ"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": "10",
+                                            "size": "sm",
+                                            "align": "end"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "รักษาอยู่"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": "10",
+                                            "size": "sm",
+                                            "align": "end"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "หายแล้ว"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": "10",
+                                            "size": "sm",
+                                            "align": "end"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "เสียชีวิต"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": "10",
+                                            "size": "sm",
+                                            "align": "end"
+                                        }
+                                    ]
+                                }
+                            ],
+                            "spacing": "sm",
+                            "margin": "xl"
                         },
                         {
-                          "type": "box",
-                          "layout": "horizontal",
-                          "margin": "xxl",
-                          "contents": [
-                            {
-                              "type": "text",
-                              "text": "ITEMS",
-                              "size": "sm",
-                              "color": "#555555"
-                            },
-                            {
-                              "type": "text",
-                              "text": "3",
-                              "size": "sm",
-                              "color": "#111111",
-                              "align": "end"
-                            }
-                          ]
+                            "type": "separator",
+                            "margin": "xxl"
                         },
                         {
-                          "type": "box",
-                          "layout": "horizontal",
-                          "contents": [
-                            {
-                              "type": "text",
-                              "text": "TOTAL",
-                              "size": "sm",
-                              "color": "#555555"
-                            },
-                            {
-                              "type": "text",
-                              "text": "$7.31",
-                              "size": "sm",
-                              "color": "#111111",
-                              "align": "end"
-                            }
-                          ]
-                        },
-                        {
-                          "type": "box",
-                          "layout": "horizontal",
-                          "contents": [
-                            {
-                              "type": "text",
-                              "text": "CASH",
-                              "size": "sm",
-                              "color": "#555555"
-                            },
-                            {
-                              "type": "text",
-                              "text": "$8.0",
-                              "size": "sm",
-                              "color": "#111111",
-                              "align": "end"
-                            }
-                          ]
-                        },
-                        {
-                          "type": "box",
-                          "layout": "horizontal",
-                          "contents": [
-                            {
-                              "type": "text",
-                              "text": "CHANGE",
-                              "size": "sm",
-                              "color": "#555555"
-                            },
-                            {
-                              "type": "text",
-                              "text": "$0.69",
-                              "size": "sm",
-                              "color": "#111111",
-                              "align": "end"
-                            }
-                          ]
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "ข้อมูลสะสม",
+                                    "align": "center"
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "ติดเชื้อ"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": "5",
+                                            "size": "sm",
+                                            "align": "end"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "รักษาอยู่"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": "5",
+                                            "size": "sm",
+                                            "align": "end"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "หายแล้ว"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": "5",
+                                            "size": "sm",
+                                            "align": "end"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "เสียชีวิต"
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": "5",
+                                            "size": "sm",
+                                            "align": "end"
+                                        }
+                                    ]
+                                }
+                            ],
+                            "spacing": "sm",
+                            "margin": "xl"
                         }
-                      ]
-                    },
-                    {
-                      "type": "separator",
-                      "margin": "xxl"
-                    },
-                    {
-                      "type": "box",
-                      "layout": "horizontal",
-                      "margin": "md",
-                      "contents": [
-                        {
-                          "type": "text",
-                          "text": "PAYMENT ID",
-                          "size": "xs",
-                          "color": "#aaaaaa"
-                        },
-                        {
-                          "type": "text",
-                          "text": "#743289384279",
-                          "color": "#aaaaaa",
-                          "size": "xs",
-                          "align": "end"
-                        }
-                      ]
-                    }
-                  ]
-                },
-                "styles": {
-                  "footer": {
-                    "separator": true
-                  }
+                    ]
                 }
             }
         };
